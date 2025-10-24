aa8f443e059a0baa6f19b3c37f71da3651a056ce
Rendy Septian Pradana <126131312+Rendyseptch@users.noreply.github.com>
2025-10-24 14:10:16 +0700

    fix:eror in db

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'auth_app',
   waitForConnections: true,
   connectionLimit: 10,
